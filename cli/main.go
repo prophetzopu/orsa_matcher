f82ebf5285a2beaa3ca7484560e7d4aac1f05430
Adam Schaub <adam.m.schaub@gmail.com>
2016-06-05 02:42:08 -0400

    Team rank structure and basic CLI

---
+++ cli/main.go
@@ -0,0 +1,278 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+    "os"
+    "math"
+    "bufio"
+    "strings"
+    "container/heap"
+    orsa "github.com/adamschaub/orsa_matcher"
+    "github.com/gocarina/gocsv"
+)
+
+const StdGroupSize int = 3
+const MaxGroupSize int = 4
+const MinGroupSize int = 2
+
+type GroupMap map[string]map[string]bool
+
+func readGroups(fileName *string) GroupMap {
+    groupFile, err := os.Open(*fileName)
+
+    if err != nil {
+        panic(err)
+    }
+    defer groupFile.Close()
+
+    scanner := bufio.NewScanner(bufio.NewReader(groupFile))
+    groups := make(GroupMap)
+    for scanner.Scan() {
+        teams := strings.Split(scanner.Text(), ",")
+        for _, thisTeam := range teams {
+            if thisTeam == "" {
+                continue
+            }
+
+            existingTeam, ok := groups[thisTeam]
+            if !ok {
+                existingTeam = make(map[string]bool)
+                groups[thisTeam] = existingTeam
+            }
+
+            for _, otherTeam := range teams {
+                if otherTeam == "" || thisTeam == otherTeam {
+                    continue
+                }
+                existingTeam[otherTeam] = true
+            }
+        }
+    }
+    return groups
+}
+
+var rankFileName = flag.String("ranks", "", "REQUIRED: CSV file with team ranks")
+var groupsFileName = flag.String("groups", "", "REQUIRED: CSV file with previous team groups")
+var platform = flag.String("platform", "pcps4", "Either pcps4 or xbox")
+var format = flag.String("format", "3v3", "One of 3v3/2v2/1v1")
+var week = flag.Int("week", 1, "The number of the week")
+
+func checkFlags() bool {
+    return *rankFileName != "" && *groupsFileName != ""
+}
+
+func PlaceExtraTeam(matches *[]orsa.MatchGroup, ranks *orsa.RankedList, groups GroupMap) {
+    team := heap.Pop(ranks).(orsa.TeamRank)
+    bestFit := 0
+    bestFitConflicts := 0
+    for i := len(*matches)-1; i >= 0; i-- {
+        match := (*matches)[i]
+        conflicts := 0
+        for _, otherTeam := range match.Teams {
+            if _, ok := groups[team.Name][otherTeam.Name]; ok {
+                conflicts++
+            }
+        }
+        if conflicts < match.N - 1 {
+            if math.Abs((*matches)[bestFit].AvgPoints - float64(team.Points)) > math.Abs(match.AvgPoints - float64(team.Points)) {
+                bestFit = i
+                bestFitConflicts = conflicts
+            }
+        }
+    }
+    if bestFitConflicts == 0 {
+        (*matches)[bestFit].Teams[(*matches)[bestFit].N] = team
+    } else {
+        insertInd := 0
+        for i, currTeam := range (*matches)[bestFit].Teams {
+            nextTeamInd := (i+1) % (*matches)[bestFit].N
+            nextTeam := (*matches)[bestFit].Teams[nextTeamInd]
+            _, okTeam1 := groups[team.Name][currTeam.Name]
+            _, okTeam2 := groups[team.Name][nextTeam.Name]
+            if !okTeam1 && !okTeam2 {
+                insertInd = nextTeamInd
+                break
+            }
+        }
+        for i := len((*matches)[bestFit].Teams)-1; i >= 0; i-- {
+            if i == insertInd {
+                (*matches)[bestFit].Teams[i] = team
+                break
+            } else {
+                (*matches)[bestFit].Teams[i] = (*matches)[bestFit].Teams[i-1]
+            }
+        }
+    }
+    (*matches)[bestFit].N++
+}
+
+func DisbandMatch (match orsa.MatchGroup, pool *orsa.RankedList) {
+    for _, t := range match.Teams {
+        if t.Name == "" { continue }
+        heap.Push(pool, t)
+    }
+}
+
+func MakeMatches(matches []orsa.MatchGroup, interleave []orsa.TeamRank, ranks *orsa.RankedList, groups GroupMap) []orsa.MatchGroup {
+    deferredTeams := []orsa.TeamRank{}
+    for ranks.Len() >= MinGroupSize && ranks.Len() > 1 {
+
+
+        var firstTeam orsa.TeamRank
+        if len(interleave) > 0 {
+            firstTeam = interleave[0]
+            interleave = interleave[1:len(interleave)]
+        } else {
+            firstTeam = heap.Pop(ranks).(orsa.TeamRank)
+        }
+        currentMatch := &orsa.MatchGroup{Teams: [4]orsa.TeamRank{firstTeam,}, N: 1, AvgPoints: float64(firstTeam.Points)}
+
+        validTeams := true
+        for ( ranks.Len() > 0 || len(interleave) > 0 ) && currentMatch.N < StdGroupSize && validTeams {
+            var team orsa.TeamRank
+            if ranks.Len() > 0 {
+                team = heap.Pop(ranks).(orsa.TeamRank)
+            } else {
+                team = interleave[0]
+                interleave = interleave[1:len(interleave)]
+            }
+
+            addTeam := true
+            for _, otherTeam := range currentMatch.Teams {
+                if otherTeam.Name == "" { continue }
+                if _, ok := groups[team.Name][otherTeam.Name]; ok {
+                    //These teams have already played. Defer the current team to a later match
+                    addTeam = false
+                    break
+                }
+            }
+            if addTeam {
+                currentMatch.Teams[currentMatch.N] = team
+                currentMatch.AvgPoints += float64(team.Points)
+                currentMatch.N++
+            } else if ranks.Len() == 0 && len(interleave) == 0 {
+                deferredTeams = append(deferredTeams, team)
+                validTeams = false
+            } else {
+                deferredTeams = append(deferredTeams, team)
+            }
+        }
+
+        //add deferred teams back into the pool
+        for _,team := range deferredTeams {
+            heap.Push(ranks, team)
+        }
+        deferredTeams = []orsa.TeamRank{}
+
+        if validTeams {
+            currentMatch.AvgPoints /= float64(currentMatch.N)
+            matches = append(matches, *currentMatch)
+        } else {
+            for _, team := range currentMatch.Teams {
+                if team.Name != "" { heap.Push(ranks, team) }
+            }
+            break
+        }
+
+        if ranks.Len() < MinGroupSize {
+            //dump the interleave if rank pool gets too low, and try again
+            for _,t := range interleave {
+                heap.Push(ranks, t)
+            }
+            interleave = []orsa.TeamRank{}
+        }
+    }
+
+    return matches
+}
+
+func PrintMatchesReddit (matches []orsa.MatchGroup) {
+    const MatchHeader string = "**Groups for Week %d of the %s %s league are as follows:**\n\n"
+    fmt.Printf(MatchHeader, *week, *format, strings.ToUpper(*platform))
+    for group, match := range matches {
+        fmt.Println(match.ToString(group+1, *week, *platform, *format))
+    }
+}
+
+func PrintMatchesFlat (matches []orsa.MatchGroup) {
+    out := ""
+    for _, match := range matches {
+        for i, t := range match.Teams {
+            out += t.Name
+            if i+1 != match.N {
+                out += ","
+            }
+        }
+        out += "\n"
+    }
+    fmt.Print(out)
+}
+
+func main() {
+    flag.Parse()
+    if ok := checkFlags(); !ok {
+        fmt.Println("One or more required flags were missing...")
+        flag.PrintDefaults()
+        return
+    }
+
+    rankFile, err := os.Open(*rankFileName)
+    if err != nil {
+        panic(err)
+    }
+    defer rankFile.Close()
+
+    ranks := &orsa.RankedList{}
+    heap.Init(ranks)
+
+    unsortedRanks := []*orsa.TeamRank{}
+    if err := gocsv.UnmarshalFile(rankFile, &unsortedRanks); err != nil {
+        panic(err)
+    }
+
+    for _, r := range unsortedRanks {
+        heap.Push(ranks, *r)
+    }
+
+    groups := readGroups(groupsFileName)
+    matches := []orsa.MatchGroup{}
+
+    maxAttempts := 11
+    attemptNum := 1
+    leftoverTeams := []orsa.TeamRank{}
+    matches = MakeMatches(matches, []orsa.TeamRank{}, ranks, groups)
+    for ranks.Len() > 1 && attemptNum < maxAttempts {
+        //move leftover teams into an interleave group
+        for ranks.Len() > 0 {
+            leftoverTeams = append(leftoverTeams, heap.Pop(ranks).(orsa.TeamRank))
+        }
+
+        //disband a number of teams
+        for i := 0; i < attemptNum; i++ {
+            if len(matches) > 0 {
+                DisbandMatch(matches[len(matches)-1], ranks)
+                matches = matches[:len(matches)-1]
+            }
+        }
+        fmt.Printf("%d leftover, %d pool\n", len(leftoverTeams), ranks.Len())
+        matches = MakeMatches(matches, leftoverTeams, ranks, groups)
+
+        leftoverTeams = []orsa.TeamRank{}
+        attemptNum++
+    }
+
+    if ranks.Len() == 1 {
+        //Add the odd man out to the first available match
+        PlaceExtraTeam(&matches, ranks, groups)
+    } else if ranks.Len() > 1 {
+        fmt.Println("Attempts ran out, and we weren't able to group everyone :(")
+    }
+
+    fmt.Println("<----------REDDIT FORMAT---------->\n")
+    PrintMatchesReddit(matches)
+    fmt.Println("\n\n<----------PLAIN GROUPS FORMAT---------->\n")
+    PrintMatchesFlat(matches)
+    fmt.Println("\n\n<----------RESULTS---------->\n")
+    fmt.Printf("%d matches, %d teams left over\n\n", len(matches), ranks.Len())
+}
---
+++ orsa_ranks.go
@@ -0,0 +1,94 @@
+package orsa
+
+import (
+    "fmt"
+    "strings"
+)
+
+type MatchGroup struct {
+    Teams [4]TeamRank
+    N   int
+    AvgPoints float64
+}
+
+func (m MatchGroup) ToString (groupNumber int, weekNumber int, platform string, format string) string {
+    const MuutBaseUrl string = "https://muut.com/arlchampionships#!"
+    const MuutPCPS4Format string = "[%s](%s/week-%d-%s-pcps4:%s)"
+    const MuutXboxFormat string = "[%s](%s/week-%d-xbox-all-formats:%s)"
+
+    outputStr := fmt.Sprintf("Group %d: ", groupNumber)
+    teamMatches := ""
+    for i, team := range m.Teams {
+        if team.Name == "" { continue }
+        //team names for match header
+        outputStr += team.Name
+        if i+1 < m.N {
+            outputStr += ", "
+        }
+
+        //team matches with forum links
+        nextTeam := (i+1) % m.N
+        teamStr := team.Name + " vs " + m.Teams[nextTeam].Name
+        //replace all spaces with hyphens
+        teamUrl := strings.Replace(strings.ToLower(teamStr), " ", "-", -1)
+
+        if len(teamUrl) > 27 {
+            teamUrl = teamUrl[:27]
+        }
+
+        teamUrl = strings.Trim(teamUrl, "-")
+
+        if platform == "pcps4" {
+            teamMatches += fmt.Sprintf(MuutPCPS4Format, teamStr, MuutBaseUrl, weekNumber, format, teamUrl)
+        } else {
+            teamMatches += fmt.Sprintf(MuutXboxFormat, teamStr, MuutBaseUrl, weekNumber, teamUrl)
+        }
+        teamMatches += "\n\n"
+    }
+    outputStr += "\n\n" + teamMatches + "\n"
+
+
+    return outputStr
+}
+
+type TeamRank struct {
+    Name    string  `csv:"Team"`
+    Played  int     `csv:"GP"`
+    Wins    int     `csv:"W"`
+    Losses  int     `csv:"L"`
+    Draws   int     `csv:"D"`
+    Points  int     `csv:"Pts"`
+    WinPercentage   string `csv:"Win Pct"`
+    GoalDiff    int `csv:"GD"`
+    GoalsFor    int `csv:"GF"`
+    GoalsAgainst    int     `csv:"GA"`
+}
+
+type RankedList []TeamRank
+
+func (r RankedList) Len() int { return len(r) }
+func (r RankedList) Less(i, j int) bool {
+    if r[i].Points < r[j].Points {
+        return false
+    } else if r[i].Points > r[j].Points {
+        return true
+    } else {
+        if r[i].GoalDiff < r[j].GoalDiff {
+            return false
+        }
+    }
+    return true
+}
+func (r RankedList) Swap(i, j int) { 
+    r[i], r[j] = r[j], r[i]
+}
+func (r *RankedList) Push(x interface{}) {
+    *r = append(*r, x.(TeamRank))
+}
+func (r *RankedList) Pop() interface{} {
+    old := *r
+    n := len(old)
+    x := old[n-1]
+    *r = old[0 : n-1]
+    return x
+}
